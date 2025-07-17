bfc5426187ccdf1777383a757f6b20a9f5c73e50
Mohammad Zohaib Siddiqui <siddiq73@sheridancollege.ca>
2025-07-17 17:01:31 -0400

    Delete Group_Project/Deliverable01/SYST17796_ProjectStarterCode directory

--- Group_Project/Deliverable01/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Main.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.project;
-
-public class Main {
-    public static void main(String[] args) {
-        MainGame game = new MainGame("Spot It - Brillux Edition");
-        game.play();
-    }
-}
-
